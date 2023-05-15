73018d3e98374cda9dea07a7492f067e7e04708b
Dmitry-Lab <poznavshii@gmail.com>
2023-05-15 17:40:00 +0400

    7 sprint - add OOP classes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { Card } from './Card.js';
+import { FormValidator } from './FormValidator.js';
+import {initialCards, config} from './constants.js';
 
 // Общие элементы
 const cardsField = document.querySelector('.elements');
@@ -26,32 +29,7 @@ const cardLinkInput = document.querySelector('.popup__form_link');
 const popupOpenImage = document.querySelector('.popup-image');
 const popupCloseImage = popupOpenImage.querySelector('.popup__close-button');
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+
 
 // Личные данные попап
 
@@ -76,35 +54,11 @@ function openPopupAddNewCard() {
     cardLinkInput.value = '';
 };
 
-function createCard(card) {
-    const newCard = document.querySelector('.card__template').content.cloneNode(true);
-    const cardName = newCard.querySelector('.elements__name');
-    cardName.textContent = card.name;
-    const cardImage = newCard.querySelector('.elements__photo');
-    cardImage.setAttribute('src', card.link);
-    cardImage.setAttribute('alt', card.name);
-    const btnLike = newCard.querySelector('.button__like');
-    btnLike.addEventListener('click', addLike);
-    const btnRemove = newCard.querySelector('.button__remove');
-    btnRemove.addEventListener('click', removeCard);
-    cardImage.addEventListener('click', openPopupImage);
-    cardsField.prepend(newCard);
-};
-
-initialCards.forEach(createCard);
-
-
-function addLike (like) {
-const activeLike = like.target;
-activeLike.classList.add('button__like-active');
-}
-
-function removeCard (event) {
-    const targetCard = event.target;
-    const card = targetCard.closest('.elements__element')
-    card.remove();
-    }
-
+initialCards.forEach((item) => {
+    const card = new Card(item, '.card__template');
+    const cardElement = card.generateCard();
+    cardsField.prepend(cardElement);
+});
 
 function addCardFormSubmit(evt) {
     evt.preventDefault();
@@ -114,39 +68,30 @@ function addCardFormSubmit(evt) {
     };
     newCard.name = cardHeaderInput.value;
     newCard.link = cardLinkInput.value;
-    initialCards.unshift(newCard);
-    createCard(initialCards[0]);
+    const card = new Card(newCard, '.card__template');
+    const cardElement = card.generateCard();
+    cardsField.prepend(cardElement);
     closeAnyPopup();
 }
-
-// Открыть картинку попап 
-
-function openPopupImage(event) {
-    const targetImage = event.target;
-    const popupImage = document.querySelector('.popup__image');
-    const popupImageHeader = document.querySelector('.popup__image_header');
-    popupImageHeader.textContent = targetImage.alt;
-    popupImage.src = targetImage.src;
-    openAnyPopup(popupOpenImage);
-};
-
 
 // Открыть-закрыть, листнеры
 
-function openAnyPopup(popup) {
+export function openAnyPopup(popup) {
     popup.classList.add('popup_opened');
     popup.addEventListener('click', closeAnyPopupOverlay);
-    document.addEventListener('keydown', closeAnyPopupEsc); 
-};
-
-function closeAnyPopupEsc (event) {
-if (event.key === 'Escape') {
-    closeAnyPopup();
+    document.addEventListener('keydown', closeAnyPopupEsc);
+    newCardPopupFormValidator.cleanInputErrors();
+    renamePopupFormValidator.cleanInputErrors();
 }
+
+function closeAnyPopupEsc(event) {
+    if (event.key === 'Escape') {
+        closeAnyPopup();
+    }
 };
 
 
-function closeAnyPopupOverlay (evt) {
+function closeAnyPopupOverlay(evt) {
     if (evt.currentTarget === evt.target) {
         closeAnyPopup();
     }
@@ -157,8 +102,7 @@ function closeAnyPopup() {
     popup.classList.remove('popup_opened');
     document.querySelector('.popup__input-error').textContent = '';
     popup.removeEventListener('click', closeAnyPopupOverlay);
-    document.removeEventListener('keydown', closeAnyPopupEsc); 
-
+    document.removeEventListener('keydown', closeAnyPopupEsc);
 };
 
 btnRename.addEventListener('click', openPopupRename);
@@ -170,4 +114,8 @@ btnPopupAddNewCardClose.addEventListener('click', closeAnyPopup);
 popupCloseImage.addEventListener('click', closeAnyPopup);
 
 
+const newCardPopupFormValidator = new FormValidator(config, formElementAddCard);
+newCardPopupFormValidator.enableValidation();
 
+const renamePopupFormValidator = new FormValidator(config, formElementRename);
+renamePopupFormValidator.enableValidation();
